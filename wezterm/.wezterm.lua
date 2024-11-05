8a1758dbab333298c1c0ad7f2c199965671a0840
John Kowalsky <john.kowalsky@petiq.com>
2024-11-05 09:43:44 -0700

    update aerospace and wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,10 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- config.color_scheme = "catppuccin-mocha"
-config.color_scheme = "Dracula (Official)"
+-- config.color_scheme = "Dracula (Official)"
+-- config.color_scheme = "rosepine"
+config.color_scheme = 'Tokyo Night'
+-- config.color_scheme = 'Tokyo Night (Gogh)'
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 14
@@ -20,6 +23,10 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
 
 config.force_reverse_video_cursor = true
+
+config.cursor_blink_rate = 800
+
+config.default_cursor_style = 'BlinkingBlock'
 
 -- and finally, return the configuration to wezterm
 return config
