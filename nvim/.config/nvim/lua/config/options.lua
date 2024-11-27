ac9ef40089ee4057fc41862fce1598546f7d6620
John Kowalsky <john.kowalsky@petiq.com>
2024-11-27 00:50:43 -0700

    set cursor to vertical bar on insert mode

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,4 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.guicursor = "n-v-c:block-Cursor/lCursor-blinkon1"
+vim.opt.guicursor = "n-v-c:block-Cursor/lCursor-blinkon1i,i-ci:ver30-iCursor-blinkwait300-blinkon200-blinkoff150"
