1c9ce7947c22a8fff927b253e63af2c99f2f87d9
jrk <jrkpetiq@gmail.com>
2025-03-27 22:41:25 -0600

    add expand/collapse all for neo-tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,20 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        never_show = {},
-      },
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        window = {
+            mappings = {
+                ["Z"] = "expand_all_nodes", -- Bind 'Z' to expand all folders recursively
+                ["z"] = "close_all_nodes", -- Optional: Bind 'z' to collapse all folders
+            },
+        },
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                never_show = { -- List patterns to always hide
+                    ".git",
+                },
+            },
+        },
     },
-  },
 }
